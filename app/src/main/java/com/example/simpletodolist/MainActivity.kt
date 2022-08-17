42a25b4d8dd633d348329e0ab5f757395cb1a449
VietNguyen <viet7896@gmail.com>
2022-08-17 16:45:05 +0700

    Final commit , change findbyId -> Binding

--- app/src/main/java/com/example/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/example/simpletodolist/MainActivity.kt
@@ -8,26 +8,29 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.simpletodolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter: TodoAdapter
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         todoAdapter = TodoAdapter(mutableListOf())
-
-        findViewById<RecyclerView>(R.id.rvTodoItems).adapter = todoAdapter
-        findViewById<RecyclerView>(R.id.rvTodoItems).layoutManager = LinearLayoutManager(this)
-        findViewById<Button>(R.id.btnAddTodo).setOnClickListener{
-            val todoTitle = findViewById<EditText>(R.id.etTodoTitle).text.toString()
+        binding.rvTodoItems.adapter = todoAdapter
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
+        binding.btnAddTodo.setOnClickListener{
+            val todoTitle = binding.etTodoTitle.text.toString()
             if(todoTitle.isNotEmpty()){
                 val todo =Todo(todoTitle)
                 todoAdapter.addTodo(todo)
-                findViewById<EditText>(R.id.etTodoTitle).text.clear()
+                binding.etTodoTitle.text.clear()
             }
         }
-        findViewById<Button>(R.id.btnDelete).setOnClickListener {
+       binding.btnDelete.setOnClickListener {
             todoAdapter.deleteDone()
         }
     }
